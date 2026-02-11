2d58537ec95ba316de45af59016aa31817cb2465
Mariusz Galan <mariusz.galan@intel.com>
2026-02-12 00:14:39 +0100

    crypto/spmd_x509: add crypto backend for mbedtls, ring algorithms
    Signed-off-by: Mariusz Galan <mariusz.galan@intel.com>

---
+++ spdmlib/src/crypto/spdm_x509/src/algorithms/ring.rs
@@ -0,0 +1,63 @@
+// Copyright (c) 2026 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0 or MIT
+
+//! Ring cryptographic backend implementation
+
+use super::{CryptoBackend, SignatureAlgorithm};
+use crate::error::{Error, Result};
+use ring::signature::{self, UnparsedPublicKey};
+
+/// Ring-based cryptographic backend
+pub struct RingBackend;
+
+impl CryptoBackend for RingBackend {
+    fn verify_signature(
+        &self,
+        algorithm: SignatureAlgorithm,
+        tbs_data: &[u8],
+        signature: &[u8],
+        public_key: &[u8],
+    ) -> Result<()> {
+        log::trace!("Verifying signature with algorithm {:?}", algorithm);
+        log::trace!("TBS data length: {}", tbs_data.len());
+        log::trace!("Signature length: {}", signature.len());
+        log::trace!("Public key length: {}", public_key.len());
+
+        // Map our algorithm enum to ring's signature verification algorithm
+        let ring_algo: &dyn signature::VerificationAlgorithm = match algorithm {
+            SignatureAlgorithm::EcdsaP256Sha256 => &signature::ECDSA_P256_SHA256_ASN1,
+            SignatureAlgorithm::EcdsaP256Sha384 => &signature::ECDSA_P256_SHA384_ASN1,
+            SignatureAlgorithm::EcdsaP384Sha256 => &signature::ECDSA_P384_SHA256_ASN1,
+            SignatureAlgorithm::EcdsaP384Sha384 => &signature::ECDSA_P384_SHA384_ASN1,
+            SignatureAlgorithm::RsaPkcs1Sha256 => &signature::RSA_PKCS1_2048_8192_SHA256,
+            SignatureAlgorithm::RsaPkcs1Sha384 => &signature::RSA_PKCS1_2048_8192_SHA384,
+            SignatureAlgorithm::RsaPkcs1Sha512 => &signature::RSA_PKCS1_2048_8192_SHA512,
+            SignatureAlgorithm::RsaPssSha256 => &signature::RSA_PSS_2048_8192_SHA256,
+            SignatureAlgorithm::RsaPssSha384 => &signature::RSA_PSS_2048_8192_SHA384,
+            SignatureAlgorithm::RsaPssSha512 => &signature::RSA_PSS_2048_8192_SHA512,
+        };
+
+        // Create an unparsed public key
+        let pk = UnparsedPublicKey::new(ring_algo, public_key);
+
+        // Verify the signature
+        pk.verify(tbs_data, signature).map_err(|e| {
+            log::error!("Signature verification failed: {:?}", e);
+            Error::SignatureError(crate::error::SignatureError::VerificationFailed)
+        })?;
+
+        log::trace!("Signature verification successful");
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ring_backend_creation() {
+        let _backend = RingBackend;
+    }
+}
